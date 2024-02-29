34c09ce454161914c93fbbe35e1c65391ae70d7c
YahyaNashar22 <yahyanashar22@gmail.com>
2024-02-29 09:15:20 +0200

    added protected routes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,100 +1,167 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import NavFooterOutlet from "./NavFooterOutlet.js"
-import Loading from '../components/Loading/Loading.js';
+import NavFooterOutlet from "./NavFooterOutlet.js";
+import Loading from "../components/Loading/Loading.js";
+import { userStore } from "../store.js";
+import ProtectedRoute from "./ProtectedRoute.js";
 
-const LazyHome = React.lazy(()=>import('../pages/Home/Home.js'));
-const LazyBody = React.lazy(()=>import('../pages/BodyPage/BodyPage.js'));
-const LazyMind = React.lazy(()=>import('../pages/MindPage/MindPage.js'));
-const LazyNutritionCenter = React.lazy(()=>import('../pages/NutritionCenterPage/NutritionCenterPage.js'));
-const LazyLogin = React.lazy(()=>import('../pages/LoginPage/LoginPage.js'));
-const LazySignup = React.lazy(()=>import('../pages/SignupPage/SignupPage.js'));
-const LazyExtraInfo = React.lazy(()=>import("../pages/ExtraInfoPage/ExtraInfoPage.js"))
-const LazyProfile = React.lazy(()=>import('../pages/ProfilePage/ProfilePage.js'));
-const LazyExercise = React.lazy(()=>import('../pages/ExercisePage/ExercisePage.js'));
-const LazySingleExercise = React.lazy(()=>import('../pages/SingleExercisePage/SingleExercisePage.js'));
-const LazyTrainingPlan = React.lazy(()=>import("../pages/TrainingPlanPage/TrainingPlanPage.js"));
-const LazySingleTrainingPlan = React.lazy(()=>import("../pages/SingleTrainingPlanPage/SingleTrainingPlanPage.js"));
-const LazyBook = React.lazy(()=>import("../pages/BookPage/BookPage.js"))
-const LazySingleBook = React.lazy(()=>import("../pages/SingleBookPage/SingleBookPage.js"))
+const LazyHome = React.lazy(() => import("../pages/Home/Home.js"));
+const LazyBody = React.lazy(() => import("../pages/BodyPage/BodyPage.js"));
+const LazyMind = React.lazy(() => import("../pages/MindPage/MindPage.js"));
+const LazyNutritionCenter = React.lazy(() =>
+  import("../pages/NutritionCenterPage/NutritionCenterPage.js")
+);
+const LazyLogin = React.lazy(() => import("../pages/LoginPage/LoginPage.js"));
+const LazySignup = React.lazy(() =>
+  import("../pages/SignupPage/SignupPage.js")
+);
+const LazyExtraInfo = React.lazy(() =>
+  import("../pages/ExtraInfoPage/ExtraInfoPage.js")
+);
+const LazyProfile = React.lazy(() =>
+  import("../pages/ProfilePage/ProfilePage.js")
+);
+const LazyExercise = React.lazy(() =>
+  import("../pages/ExercisePage/ExercisePage.js")
+);
+const LazySingleExercise = React.lazy(() =>
+  import("../pages/SingleExercisePage/SingleExercisePage.js")
+);
+const LazyTrainingPlan = React.lazy(() =>
+  import("../pages/TrainingPlanPage/TrainingPlanPage.js")
+);
+const LazySingleTrainingPlan = React.lazy(() =>
+  import("../pages/SingleTrainingPlanPage/SingleTrainingPlanPage.js")
+);
+const LazyBook = React.lazy(() => import("../pages/BookPage/BookPage.js"));
+const LazySingleBook = React.lazy(() =>
+  import("../pages/SingleBookPage/SingleBookPage.js")
+);
 
 function AppRoutes() {
+
+  const { user } = userStore();
+  
   return (
     <Routes>
-        <Route exact path='/' element={<NavFooterOutlet />}>
-            <Route path='/' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyHome />
-              </React.Suspense>
-            }/>
-            <Route path='/body' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyBody />
-              </React.Suspense>
-            }/>
-            <Route path='/mind' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyMind />
-              </React.Suspense>
-            }/>
-            <Route path='/nutrition-center' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyNutritionCenter />
-              </React.Suspense>
-            }/>
-            <Route path='/exercises' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyExercise />
-              </React.Suspense>
-            }/>
-            <Route path='/singleexercise/:id' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazySingleExercise />
-              </React.Suspense>
-            }/>
-            <Route path='/trainingplans' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyTrainingPlan />
-              </React.Suspense>
-            }/>
-            <Route path='/singletrainingplan/:id' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazySingleTrainingPlan />
-              </React.Suspense>
-            }/>
-            <Route path='/profile' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyProfile />
-              </React.Suspense>
-            }/>
-            <Route path='/books' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyBook />
-              </React.Suspense>
-            }/>
-            <Route path='/singlebook/:id' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazySingleBook />
-              </React.Suspense>
-            }/>
+      <Route exact path="/" element={<NavFooterOutlet />}>
+        <Route
+          path="/"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazyHome />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/nutrition-center"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazyNutritionCenter />
+            </React.Suspense>
+          }
+        />
+        <Route element={<ProtectedRoute />}>
+        <Route
+          path="/body"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazyBody />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/mind"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazyMind />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/exercises"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazyExercise />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/singleexercise/:id"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazySingleExercise />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/trainingplans"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazyTrainingPlan />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/singletrainingplan/:id"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazySingleTrainingPlan />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazyProfile />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/books"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazyBook />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/singlebook/:id"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <LazySingleBook />
+            </React.Suspense>
+          }
+        />
         </Route>
-        <Route path='/login' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyLogin />
-              </React.Suspense>
-            }/>
-            <Route path='/signup' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazySignup />
-              </React.Suspense>
-            }/>
-            <Route path='/extrainfo' element={
-              <React.Suspense fallback={<Loading />}>
-                <LazyExtraInfo />
-              </React.Suspense>
-            }/>
+      </Route>
+      <Route
+        path="/login"
+        element={
+          <React.Suspense fallback={<Loading />}>
+            <LazyLogin />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <React.Suspense fallback={<Loading />}>
+            <LazySignup />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="/extrainfo"
+        element={
+          <React.Suspense fallback={<Loading />}>
+            <LazyExtraInfo />
+          </React.Suspense>
+        }
+      />
     </Routes>
-  )
+  );
 }
 
-export default AppRoutes
+export default AppRoutes;
---
+++ src/routes/ProtectedRoute.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { userStore } from '../store'
+import { Navigate, Outlet } from "react-router-dom";
+
+function ProtectedRoute() {
+
+    const {user} = userStore(); 
+
+    if(Object.keys(user).length<1){
+        return <Navigate to='/login' replace />
+    }
+
+    if(user && !user.gender){
+        return <Navigate to='/extrainfo' replace />
+    }
+
+  return (
+    <Outlet />
+  )
+}
+
+export default ProtectedRoute
